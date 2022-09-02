f230b71b7662194a6aa0d772dc022ad4cca9ec9f
Henry Do <hpdo@student.unimelb.edu.au>
2022-09-02 20:58:01 +1000

    client side signup validation

--- client/Signup.js
+++ client/Signup.js
@@ -1,2 +1,9 @@
-import React from 'react';
-
+function validateForm() {
+    const pass1 = document.forms["signupForm"]["password"];
+    const pass2 = document.forms["signupForm"]["confPassword"];
+    
+    if (pass1 != pass2) {
+        alert("Passwords need to be the same");
+        return false;
+    }
+}

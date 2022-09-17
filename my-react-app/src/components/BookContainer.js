9f87c20415e930807e354fc465e0ab3aacd14602
ThatGuyXD <chickenbacon97@gmail.com>
2022-09-18 02:14:34 +1000

    book container is horizontally scrollable and centered on home page

--- my-react-app/src/components/BookContainer.js
+++ my-react-app/src/components/BookContainer.js
@@ -3,8 +3,21 @@ import PropTypes from 'prop-types';
 import Book from "./Book";
 import style from "./CSS/bookContainer.module.css"
 export default function BookContainer({Books}) {
+    const scrollStrength = 100;
+    const container = document.getElementById("BookContainer");
+    container.addEventListener("wheel", function(e) {
+        if(e.deltaY > 0) {
+            container.scrollLeft += scrollStrength;
+            e.preventDefault();
+        }
+        else {
+            container.scrollLeft -= scrollStrength;
+            e.preventDefault();
+        }
+    });
+
     return (
-        <ul className={style.BookContainerItems}>
+        <ul className={style.BookContainerItems} id="BookContainer">
             {Books.map(Books=> 
                 <li key={Books.id} className={style.BookContainerItem}>
                     <Book books={Books}/>
--- my-react-app/src/components/StaticData/BooksData.js
+++ my-react-app/src/components/StaticData/BooksData.js
@@ -4,12 +4,57 @@ let books = [
     {
         id: 0,
         image: book1,
-        alttext: "book1"
+        alttext: "book0"
     },
     {
         id: 1,
         image: book1 ,
+        alttext: "book1"
+    },
+    {
+        id: 2,
+        image: book1 ,
         alttext: "book2"
+    },
+    {
+        id: 3,
+        image: book1 ,
+        alttext: "book3"
+    },
+    {
+        id: 4,
+        image: book1 ,
+        alttext: "book4"
+    },
+    {
+        id: 5,
+        image: book1 ,
+        alttext: "book5"
+    },
+    {
+        id: 6,
+        image: book1 ,
+        alttext: "book6"
+    },
+    {
+        id: 7,
+        image: book1 ,
+        alttext: "book7"
+    },
+    {
+        id: 8,
+        image: book1 ,
+        alttext: "book8"
+    },
+    {
+        id: 9,
+        image: book1 ,
+        alttext: "book9"
+    },
+    {
+        id: 10,
+        image: book1 ,
+        alttext: "book10"
     }
 ]
 

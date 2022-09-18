403ae4c15d26c754b9ea33b1c6c223fe1ac5aff4
Ian Nguyen <chickenbacon97@gmail.com>
2022-09-18 14:50:43 +1000

    fixed category image sizes on chrome for laptop screens

--- my-react-app/src/components/BookContainer.js
+++ my-react-app/src/components/BookContainer.js
@@ -1,29 +1,40 @@
 import React from "react";
 import PropTypes from 'prop-types';
 import Book from "./Book";
-import style from "./CSS/bookContainer.module.css"
+import style from "./CSS/bookContainer.module.css";
+import leftArrow from "./Images/leftarrow.png";
+import rightArrow from "./Images/rightarrow.png";
+
 export default function BookContainer({Books}) {
     const scrollStrength = 100;
-    const container = document.getElementById("BookContainer");
-    container.addEventListener("wheel", function(e) {
-        if(e.deltaY > 0) {
-            container.scrollLeft += scrollStrength;
-            e.preventDefault();
-        }
-        else {
-            container.scrollLeft -= scrollStrength;
-            e.preventDefault();
-        }
-    });
+    const container = document.getElementById("BookRow");
+    if(container != null)
+    {
+        container.addEventListener("wheel", function(e) {
+            if(e.deltaY > 0) {
+                container.scrollLeft += scrollStrength;
+                e.preventDefault();
+            }
+            else {
+                container.scrollLeft -= scrollStrength;
+                e.preventDefault();
+            }
+        });
+    }
+
 
     return (
-        <ul className={style.BookContainerItems} id="BookContainer">
-            {Books.map(Books=> 
-                <li key={Books.id} className={style.BookContainerItem}>
-                    <Book books={Books}/>
-                </li>    
-            )}
-        </ul>
+        <div>
+            <img className="Arrow" id="leftArrow" src={leftArrow} alt="leftArrow"/>
+            <ul className={style.BookContainerItems} id="BookRow">
+                {Books.map(Books=> 
+                    <li key={Books.id} className={style.BookContainerItem}>
+                        <Book books={Books}/>
+                    </li>    
+                )}
+            </ul>
+            <img className="Arrow" id="leftArrow" src={rightArrow} alt="rightArrow"/>
+        </div>
     );
 }
 

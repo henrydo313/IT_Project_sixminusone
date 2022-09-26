7df24da931cc8ed7fbbb3b3ae4dcc2ba37e12732
kind0fBlue <hcy3766@gmail.com>
2022-09-26 21:15:36 +1000

    admin page added

---
+++ myobject/myadmin/views/index.py
@@ -0,0 +1,7 @@
+# myobject/myadmin/views/index.py
+from django.shortcuts import render
+from django.http import HttpResponse
+
+#后台首页
+def index(request):
+    return render(request,"myadmin/index/index.html")

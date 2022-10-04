be340e6249998b5883c6224c121c85a04d835c3c
Henry Do <henrydo313@gmail.com>
2022-10-04 14:57:02 +1100

    new folder for static file error

--- myobject/myobject/settings.py
+++ myobject/myobject/settings.py
@@ -11,8 +11,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-import pymysql  
-pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -84,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'osdb',
         'USER': 'root',
-        'PASSWORD': '37667616',
+        'PASSWORD': '123456',
         'HOST': 'localhost',
         'PORT': '3306',
     }

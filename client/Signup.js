e39f47c429fbfca366574260c4646e731088bcb3
Henry Do <henrydo313@gmail.com>
2022-10-11 14:38:25 +1100

    merge branch

--- client/Signup.js
+++
@@ -1,9 +0,0 @@
-function validateForm() {
-    const pwd = document.forms['signupForm']['passwordInput'].value;
-    const confPwd = document.forms['signupForm']['passwordConfirmInput'].value;
-
-    if (pwd != confPwd) {
-        alert("Passwords don't match");
-        return false;
-    }
-}

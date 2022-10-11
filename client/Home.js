25004837deddf1be55a77b4e35b549da915fb83d
Ian Nguyen <chickenbacon97@gmail.com>
2022-10-11 15:26:20 +1100

    added a script for scrolling with buttons for the homepage

--- client/Home.js
+++ client/Home.js
@@ -4,17 +4,17 @@ var leftArrowLat = document.getElementById('leftArrowLatest');
 var rightArrowLat = document.getElementById('rightArrowLatest');
 
 leftArrowPop.onclick = function() {
-    document.getElementById("containerPop").scrollLeft += 20;
+    document.getElementById("containerPop").scrollLeft -= 200;
 }
 
 rightArrowPop.onclick = function() {
-    document.getElementById("containerPop").scrollLeft -= 20;
+    document.getElementById("containerPop").scrollLeft += 200;
 }
 
 leftArrowLat.onclick = function() {
-    document.getElementById("containerLatest").scrollLeft += 20;
+    document.getElementById("containerLatest").scrollLeft -= 200;
 }
 
 rightArrowLat.onclick = function() {
-    document.getElementById("containerLatest").scrollLeft -= 20;
+    document.getElementById("containerLatest").scrollLeft += 200;
 }

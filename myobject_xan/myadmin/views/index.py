3fe971147974be7245432ab2b109e522686aa21c
jiahongg <88899688+jiahongg@users.noreply.github.com>
2022-10-11 15:31:37 +1100

    Merge

--- myobject_xan/myadmin/views/index.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-#后台首页
-def index(request):
-    return render(request,"myadmin/index/index.html")
--- myobject_xan/web/views/index.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-#前台首页
-def index(request):
-    return HttpResponse('Welcome to the front page')
